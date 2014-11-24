93667925de802a374067891b6139a15612b3991f
Rob Desbois <rob.desbois@gmail.com>
2014-11-24 13:06:33 +0000

    Extract #include path from lines when selection is empty

--- sublime-plugins/goto_selected_file.py
+++ sublime-plugins/goto_selected_file.py
@@ -1,15 +1,30 @@
 import sublime, sublime_plugin
+import re
 
 # Opens the 'goto file' overlay with selected text as initial search
+# Will also inspect the selection (or current line if no selection) and pull out
+# header paths from #include directives
 class GotoSelectedFileCommand(sublime_plugin.WindowCommand):
+
     def run(self):
         view = self.window.active_view()
         sel  = view.sel()
 
         searchText = ''
-        if len(sel):
-           searchText = view.substr(sel[0])
-           
+        if len(sel) and len(sel[0]):
+            searchText = view.substr(sel[0])
+        else:
+            # grab the whole line text
+            searchRegion = view.line(sel[0])
+            searchText   = view.substr(searchRegion)
+
+            # test for #include & extract path
+            match = re.match('#\s*include\s*["<]([^">]+)[">]', searchText)
+            if match:
+                searchText = match.group(1)
+            else:
+                searchText = ''
+
         self.window.run_command("show_overlay", {"overlay":    "goto",
                                                  "show_files": True,
                                                  "text":       searchText})
